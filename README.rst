687f98b86a92dbfbe22afe4ded8d5f60d9b23dfe
apra <dd.apra93@gmail.com>
2018-04-17 18:53:36 -0700

    ENH: Adding the basic structure.

---
+++ README.rst
@@ -0,0 +1,14 @@
+=============
+Apra Dotfiles
+=============
+Currently under construction...
+
+
+Acknowledgements
+----------------
+- Dotfile setup is powered by `dotbot <https://git.io/dotbot>`_.
+
+License
+-------
+Copyright (c) 2017-2018 Abdullah P Rashed Ahmed. Released under the MIT License. See LICENSE.md for details.
+
---
+++ shell/common.sh
@@ -0,0 +1,31 @@
+# Common setup for various shells
+
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# We will need miniconda for all setups
+export PATH="/home/apra/miniconda3/bin:$PATH"
+
+# Set the x11 display. Make sure the x11 server is running!
+export DISPLAY=:0
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+    export EDITOR='emacs -nw'
+else
+    export EDITOR='emacs -nw'
+fi
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/rsa_id"
+
+# Basic prompt
+export PS1='${PWD#"${PWD%/*/*}/"} \$ '
