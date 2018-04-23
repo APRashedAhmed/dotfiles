abcaacd341143989cc1b2950cd75f66c8c7af318
apra <dd.apra93@gmail.com>
2018-04-23 00:18:19 -0700

    FIX: Removing zsh alias from common aliases.

--- common/aliases.sh
+++ common/aliases.sh
@@ -32,20 +32,6 @@ alias findd='find . -type d -name'
 alias findf='find . -type f -name'
 
 
-# # Downloading
-
-# Stolen from zim/utilities (along with some other stuff)
-if (( ${+commands[aria2c]} )); then
-  alias get='aria2c --max-connection-per-server=5 --continue'
-elif (( ${+commands[axel]} )); then
-  alias get='axel --num-connections=5 --alternate'
-elif (( ${+commands[wget]} )); then
-  alias get='wget --continue --progress=bar --timestamping'
-elif (( ${+commands[curl]} )); then
-  alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
-fi
-
-
 # # Resource Usage
 
 # 1k block size and human readable
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,17 @@
 
 # Reload zshrc
 alias reload='. ~/.zshrc'
+
+# # Downloading
+
+# Stolen from zim/utilities (along with some other stuff)
+if (( ${+commands[aria2c]} )); then
+  alias get='aria2c --max-connection-per-server=5 --continue'
+elif (( ${+commands[axel]} )); then
+  alias get='axel --num-connections=5 --alternate'
+elif (( ${+commands[wget]} )); then
+  alias get='wget --continue --progress=bar --timestamping'
+elif (( ${+commands[curl]} )); then
+  alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
+fi
+
