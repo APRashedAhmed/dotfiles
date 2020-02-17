ad96266d0be4e23932b73c4f937fa59c11326aff
apra <dd.apra93@gmail.com>
2020-02-17 02:01:22 -0500

    GRP: Using too much space with local miniconda.

--- groups/serre.sh
+++ groups/serre.sh
@@ -25,19 +25,37 @@ alias x9='ssh abdullah@$x9'
 alias nsmi='nvidia-smi'
 
 # This only applies to machines which can see data_cifs
-if globhost clps.brown.edu; then
+if globhost brown.edu; then
+    export CONDA_PKGS_DIRS="/media/data/conda/pkgs"
+    export CONDA_ENVS_DIRS="/media/data/conda/abdullah/envs"
     # >>> conda initialize >>>
     # !! Contents within this block are managed by 'conda init' !!
-    __conda_setup="$('/home/abdullah/data_cifs/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    __conda_setup="$('/media/data/anaconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
 	eval "$__conda_setup"
     else
-	if [ -f "/home/abdullah/data_cifs/miniconda3/etc/profile.d/conda.sh" ]; then
-	    . "/home/abdullah/data_cifs/miniconda3/etc/profile.d/conda.sh"
+	if [ -f "/media/data/anaconda/etc/profile.d/conda.sh" ]; then
+	    . "/media/data/anaconda/etc/profile.d/conda.sh"
 	else
-	    export PATH="/home/abdullah/data_cifs/miniconda3/bin:$PATH"
+	    export PATH="/media/data/anaconda/bin:$PATH"
 	fi
     fi
     unset __conda_setup
     # <<< conda initialize <<<
 fi
+
+#     # >>> conda initialize >>>
+#     # !! Contents within this block are managed by 'conda init' !!
+#     __conda_setup="$('/home/abdullah/data_cifs/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+#     if [ $? -eq 0 ]; then
+# 	eval "$__conda_setup"
+#     else
+# 	if [ -f "/home/abdullah/data_cifs/miniconda3/etc/profile.d/conda.sh" ]; then
+# 	    . "/home/abdullah/data_cifs/miniconda3/etc/profile.d/conda.sh"
+# 	else
+# 	    export PATH="/home/abdullah/data_cifs/miniconda3/bin:$PATH"
+# 	fi
+#     fi
+#     unset __conda_setup
+#     # <<< conda initialize <<<
+# fi
