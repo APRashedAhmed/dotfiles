e182a7cba0640c5c86efebb973e30222afc145df
Abdullah <aprashedahmed@gmail.com>
2024-04-08 02:10:59 -0400

    New emacs and nvm additions

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -26,3 +26,7 @@ source $DOTPATH/groups/brown.sh
 # Source local zsh setup if it exists
 [[ -f ~/.local/.zshrc ]] && source ~/.local/.zshrc
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
